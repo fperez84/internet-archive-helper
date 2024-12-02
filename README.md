7624f03fbc1943da25a62dbb3647c27880213b88
Francisco Perez <fperez.x@gmail.com>
2024-12-02 10:26:28 -0300

    Adding documentation

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
-# internet-archive-helper
+# Internet Archive Helper
+
+This script is done literally for education purpose and also fun. I knew old
+Club Nintendo Chile magazines are available at [Archive.org](https://archive.org),
+but there are so many to do click-ops so I thought of automating this using python
+(Plus, I'm looking to refine my python skills so this seems like the perfect
+opportunity).
+
+The script will perform a search on [Archive.org](https://archive.org) for
+'Club Nintendo' and filter the results for Chilean issues only. Then it will
+download them in a given directory.
+
+For the above, the script is using internetarchive library and to prevent ban
+is taking a two seconds delay between downloads.
+
+## Features
+
+- Searches Archive.org using the `internetarchive` library.
+- Filters results by identifiers containing "chile" (case-insensitive).
+- Downloads only PDF files, excluding those with the suffix `_text.pdf`.
+- Allows configuring the destination directory for downloaded files.
+- Implements a delay between downloads to avoid API rate limits.
+
+---
+
+## Installation
+
+### Prerequisites
+
+- Python 3.7 or higher.
+- Internet access.
+
+### Installing dependencies
+
+1. Clone this repository:
+
+   ```bash
+   git clone https://github.com/fperez84/internet-archive-helper.git
+   cd internet-archive-helper
+    ```
+
+1. Install the dependencies
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## Usage
+
+1. Set the destination directory in the project's main file (main.py):
+
+    ```python
+    output_dir = Path("/your/destination/path")
+    ```
+
+1. Run the script
+
+    ```bash
+    python main.py
+    ```
+
+1. All downloaded files will be on output_dir
+
+## Dependencies
+
+- internetarchive: For interacting with Archive.org's API.
+- pathlib: Advanced handling of paths and directories.
+- time: Adds delays between downloads.
+
+## Future Improvements
+
+- Add automatic handling of HTTP errors.
+- Implement configuration options via a .env file or command-line arguments.
+
+## License
+
+This project is licensed under the BSD 3-Clause License. See the LICENSE file
+for more details.
+
+## Note
+
+This project is for educational purposes only and has no commercial intent.
+Please respect copyright laws and Archive.org's usage policies.
--- main.py
+++ main.py
@@ -1,52 +1,58 @@
 """
-Este script realiza una búsqueda en Archive.org utilizando la librería `internetarchive`, 
-filtra los resultados que contienen 'chile' en su identificador, 
-y descarga los archivos PDF relacionados a "Club Nintendo" en un directorio especificado.
+This script is done literally for education purpose and also fun. I knew old
+Club Nintendo Chile magazines are available at archive.org, but there are so
+many to do click-ops so I thought of automating this using python(Plus, I'm
+looking to refine my python skills so this seems like the perfect opportunity).
 
-El proceso incluye un retraso entre descargas para evitar bloqueos de la API.
+The script will perform a search on archive.org for 'Club Nintendo' and filter
+the results for Chilean issues only. Then it will download them in a given
+directory.
+
+For the above, the script is using internetarchive library and to prevent ban
+is taking a two seconds delay between downloads.
 """
 import time
 
 from pathlib import Path
 from internetarchive import search_items, get_item
 
-# Inicializar arreglo de resultados filtrados
+# Variables init
 filtered_results = []
-output_dir = Path("/tmp/Club Nintendo")  # Directorio donde se guardarán los archivos
-DELAY = 2  # Retraso en segundos entre descargas
+output_dir = Path("/tmp/Club Nintendo")  # Download Directory
+DELAY = 2  # Delay in seconds between downloads
 
-# Realizar búsqueda de "Club Nintendo"
+# Search for Club Nintendo, key item here is do a title search.
 for item in search_items('title:Club Nintendo'):
-    # Verificar que el título y el identificador existan
-    identifier = item.get('identifier')  # Recuperar el identificador
+    # From all identifier returned, filter Chilean issues.
+    identifier = item.get('identifier')
     if "chile" in identifier.lower():
         filtered_results.append(identifier)
 
-# Mostrar la cantidad de resultados filtrados
-print(f"Cantidad de resultados filtrados: {len(filtered_results)}")
-
-# Proceso de descarga
-# Crear el directorio
+# Download
+# Create direcroty if it doesn't exists. With all parent folders
 try:
     output_dir.mkdir(parents=True, exist_ok=True)
-    print(f"Directorio creado en: {output_dir}")
+    print(f"Directory created at: {output_dir}")
 except Exception as e:
-    print(f"Error al crear el directorio: {e}")
+    print(f"Error on directory creation: {e}")
 
+# In order to provide progresion to the user we enumerate the filtered_results
 for index, item_id in enumerate(filtered_results, start=1):
-    print(f"[{index}/{len(filtered_results)}] Descargando el item: {item_id}")
+    print(f"[{index}/{len(filtered_results)}] Downloading item: {item_id}")
     try:
         item = get_item(item_id)
-        # Filtrar y descargar solo archivos .pdf que no contengan '_text' en su nombre
+        # internetarchive from each identifier returns an item that could have
+        # more than one file, in our case there are 2 versions of each magazine
+        # the one that ends with _text seems to be for mobile view, which I'm
+        # not looking for, that's why Im filtering those out.
         files = item.files
         for file in files:
             if file['name'].endswith('.pdf') and '_text' not in file['name']:
-                # Descargar el archivo .pdf si no tiene '_text' en el nombre
                 item.download(glob_pattern=file['name'], destdir=output_dir, verbose=True)
-                print(f"✔ Descarga completada para: {file['name']}")
+                print(f"✔ Download completed for: {file['name']}")
     except Exception as e:
-        print(f"❌ Error al descargar {item_id}: {e}")
-    # Retraso entre descargas
+        print(f"❌ Download error at: {item_id}: {e}")
+    # Delay between each download.
     time.sleep(DELAY)
 
-print("🎉 Todas las descargas han finalizado.")
+print("🎉 All downloads finished!.")
